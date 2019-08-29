5c550049d6004444201051fdf0411bdcc4186bc4
Jon Gorman <contact@jongorman.top>
2019-08-28 23:21:10 -0500

    working but needs improvement

--- timeTesting.js
+++ timeTesting.js
@@ -1,4 +1,4 @@
-firstHourArray= []
+ firstHourArray= []
 function getHourArray(){
   let now = new Date();
   let hour = now.getHours()
@@ -8,126 +8,124 @@ function getHourArray(){
     console.log(firstHourArray);
     return firstHourArray
   }
-  else if((hour-11===0)){
+   if((hour-11===0)){
     firstHourArray=[5, 3, 2, 1, ''];
     console.log(firstHourArray);
     return firstHourArray
   }
-  else if((hour-10===0)){
+   if((hour-10===0)){
     firstHourArray=[5, 3, 2, '', ''];
     console.log(firstHourArray);
     return firstHourArray
   }
-  else if((hour-9===0)){
+   if((hour-9===0)){
     firstHourArray=[5, 3, '', 1, ''];
     console.log(firstHourArray);
     return firstHourArray
   }
-  else if((hour-8===0)){
+   if((hour-8===0)){
     firstHourArray=[5, 3, '', '', ''];
     console.log(firstHourArray)
     return firstHourArray
   }
-  else if((hour-7===0)){
+   if((hour-7===0)){
      firstHourArray=[5, '', 2, '', ''];
     console.log(firstHourArray);
     return firstHourArray
   }
-  else if((hour-6===0)){
+   if((hour-6===0)){
     firstHourArray=[5, '', '', 1, ''];
     console.log(firstHourArray);
     return firstHourArray
   }
-  else if((hour-5===0)){
+   if((hour-5===0)){
     firstHourArray=[5, '','','',''];
     console.log(firstHourArray );
     return firstHourArray
   }
-  else if((hour-4===0)){
+   if((hour-4===0)){
     firstHourArray=['', 3, '', 1, ''];
     console.log(firstHourArray);
     return firstHourArray
   }
-  else if((hour-3===0)){
+   if((hour-3===0)){
     firstHourArray=['', 3, '', '', ''];
     console.log(firstHourArray);
     return firstHourArray
   }
-  else if((hour-2===0)){
+   if((hour-2===0)){
     firstHourArray=['','',2,'',''];
     console.log(firstHourArray);
     return firstHourArray
   }
-  else if((hour-1===0)){
-    firstHourArrayy=['', '', '', 1, ''];
+   if((hour-1===0)){
+    firstHourArray=['', '', '', 1, ''];
     console.log(firstHourArray);
     return firstHourArray
   }
-  else if((hour-24===0)){
+   if((hour-24===0)){
     firstHourArray=[5, 3, 2, 1, 1];
     console.log(firstHourArray);
     return firstHourArray
   }
-  else if((hour-23===0)){
+   if((hour-23===0)){
     firstHourArray=[5, 3, 2, 1, ''];
     console.log(firstHourArray);
     return firstHourArray
   }
-  else if((hour-22===0)){
+   if((hour-22===0)){
     firstHourArray=[5, 3, 2, '', ''];
     console.log(firstHourArray);
     return firstHourArray
   }
-  else if((hour-21===0)){
+   if((hour-21===0)){
     firstHourArray=[5, 3, '', 1, ''];
     console.log(firstHourArray);
     return firstHourArray
   }
-  else if((hour-20===0)){
+   if((hour-20===0)){
     firstHourArray=[5, 3, '', '', ''];
     console.log(firstHourArray)
     return firstHourArray
   }
-  else if((hour-19===0)){
+   if((hour-19===0)){
     firstHourArray=[5, '', 2, '', ''];
     console.log(firstHourArray);
     return firstHourArray
   }
-  else if((hour-18===0)){
+   if((hour-18===0)){
     firstHourArray=[5, '', '', 1, ''];
     console.log(firstHourArray);
     return firstHourArray
   }
-  else if((hour-17===0)){
+   if((hour-17===0)){
     firstHourArray=[5, '','','',''];
     console.log(firstHourArray );
     return firstHourArray
   }
-  else if((hour-16===0)){
+   if((hour-16===0)){
     firstHourArray=['', 3, '', 1, ''];
     console.log(firstHourArray);
     return firstHourArray
   }
-  else if((hour-15===0)){
+   if((hour-15===0)){
     firstHourArray=['', 3, '', '', ''];
     console.log(firstHourArray);
     return firstHourArray
   }
-  else if((hour-14===0)){
+   if((hour-14===0)){
     firstHourArray=['','',2,'',''];
     console.log(firstHourArray);
     return firstHourArray
   }
-  else if((hour-13===0)){
-    firstHourArrayy=['', '', '', 1, ''];
+   if((hour-13===0)){
+    firstHourArray=['', '', '', 1, ''];
     console.log(firstHourArray);
     return firstHourArray
   }
-
 }
 
 firstMinuteArray = [];
-
 function getMinuteArray(){
   let now = new Date();
   let minute = Math.floor(now.getMinutes()/5);
@@ -137,183 +135,60 @@ function getMinuteArray(){
     console.log(firstMinuteArray);
     return firstMinuteArray
   }
-  else if(minute===1){
+   if(minute===1){
     firstMinuteArray = ['', '', '', 1,''];
     console.log(firstMinuteArray);
     return firstMinuteArray
   }
-  else if(minute===2){
+   if(minute===2){
     firstMinuteArray = ['','',2,'','']
     console.log(firstMinuteArray);
     return firstMinuteArray
   }
-  else if(minute===3){
+   if(minute===3){
     firstMinuteArray = ['',3,'','','']
     console.log(firstMinuteArray);
     return firstMinuteArray
   }
-  else if(minute===4){
+   if(minute===4){
     firstMinuteArray = ['', 3, '', 1, '']
     console.log(firstMinuteArray);
     return firstMinuteArray
   }
-  else if(minute===5){
+   if(minute===5){
     firstMinuteArray = [5,'','','','']
     console.log(firstMinuteArray);
     return firstMinuteArray
   }
-  else if(minute===6){
+   if(minute===6){
     firstMinuteArray = [5, '', '', 1, '']
     console.log(firstMinuteArray);
     return firstMinuteArray
   }
-  else if(minute===7){
+   if(minute===7){
     firstMinuteArray = [5, 2,'','','']
     console.log(firstMinuteArray);
     return firstMinuteArray
   }
-  else if(minute===8){
+   if(minute===8){
     firstMinuteArray = [5, 3,'','','']
     console.log(firstMinuteArray);
     return firstMinuteArray
   }
-  else if(minute===9){
+   if(minute===9){
     firstMinuteArray = [5, 3, '', 1, '']
     console.log(firstMinuteArray);
     return firstMinuteArray
   }
-  else if(minute===10){
+   if(minute===10){
     firstMinuteArray = [5, 3, 2,'','']
     console.log(firstMinuteArray);
     return firstMinuteArray
   }
-  else if(minute===11){
+   if(minute===11){
     firstMinuteArray = [5, 3, 2, 1,'']
     console.log(firstMinuteArray);
     return firstMinuteArray
   }
 
 }
-
-
-
-
-
-
-
-// function setHour(){
-//   const now = new Date();
-//   let hour = now.getHours();
-//   console.log(hour, "hours!");
-//   //case for pm times
-//   if(hour >= 13){
-//       let pmHour = hour-12;
-//       console.log(pmHour)
-//       return pmHour
-//   }
-//   //case for 12 am
-//   if (hour === 0){
-//     hourArray.push([5,3,2,1,1])
-//     console.log(hour, "12 o'clock")
-//     return hour
-//   }
-//   return hour
-// }setInterval(setHour, 150000);
-//
-// const onesHourArray = []
-// const hourArray = [];
-// let firstHourArray = []
-// function timeSumHour(fibArray, H) {
-//
-//   for(let i = 0; i< fibArray.length; i++) {
-//     if(H==fibArray[i]){
-//       hourArray.push([fibArray[i]])
-//     }
-//     for(let j = i+1;  j < fibArray.length; j++) {
-//       if(H==fibArray[i] + fibArray[j]) {
-//         hourArray.push([fibArray[i], fibArray[j]])
-//       }
-//       for(let k = i+1+1; k < fibArray.length; k++){
-//         if (H == fibArray[i] + fibArray[j] + fibArray[k]){
-//           hourArray.push([fibArray[i], fibArray[j], fibArray[k]])
-//         }
-//         for(let l = i+1+1+1; l<fibArray.length; l++){
-//           if (H == fibArray[i] + fibArray[j] + fibArray[k] + fibArray[l]){
-//             hourArray.push([fibArray[i], fibArray[j], fibArray[k], fibArray[l]])
-//           }
-//           for(let m = i+1+1+1+1; m<fibArray.length; m++) {
-//             if (H == fibArray[i] + fibArray[j] + fibArray[k] + fibArray[l] + fibArray[m]) {
-//               hourArray.push([fibArray[i], fibArray[j], fibArray[k], fibArray[l], fibArray[m]])
-//               }
-//             }
-//           }
-//         }
-//       }
-//     }
-//   //Checking for the ones
-//   for(let i=0; i < hourArray[0].length; i++){
-//     if(hourArray[0][i] === 1){
-//       onesHourArray.push(hourArray[0][i] = 1)
-//     }
-//   }console.log(onesHourArray, "ones hours")
-//
-//     for(let i=0; i < hourArray[0].length; i++){
-//       firstHourArray.push(hourArray[0][i])
-//     }
-//   console.log(firstHourArray, "this is 1st hour Array")
-//   return firstHourArray
-// }
-//
-//
-// function setMinute(){
-//   const now = new Date();
-//   const minute = now.getMinutes()
-//   console.log(minute, "first console of minute")
-//   const seconds = now.getSeconds();
-//   console.log(Math.floor(minute/5) + '.' + seconds)
-//   let rounded = Math.floor(minute/5);
-//   return rounded
-// }setInterval(setMinute, 1000);
-//
-// const onesMinuteArray = []
-// const minuteArray = [];
-// const firstMinuteArray = [];
-// function timeSumMinute(fibArray, M) {
-//   for(let i = 0; i< fibArray.length; i++) {
-//     if(M==fibArray[i]){
-//       minuteArray.push([fibArray[i]])
-//     }
-//     for(let j = i+1;  j < fibArray.length; j++) {
-//       if(M==fibArray[i] + fibArray[j]) {
-//         minuteArray.push([fibArray[i], fibArray[j]])
-//       }
-//       for(let k = i+1+1; k < fibArray.length; k++){
-//         if (M == fibArray[i] + fibArray[j] + fibArray[k]){
-//           minuteArray.push([fibArray[i], fibArray[j], fibArray[k]])
-//         }
-//         for(let l = i+1+1+1; l<fibArray.length; l++){
-//           if (M == fibArray[i] + fibArray[j] + fibArray[k] + fibArray[l]){
-//             minuteArray.push([fibArray[i], fibArray[j], fibArray[k], fibArray[l]])
-//           }
-//           for(let m = i+1+1+1+1; m<fibArray.length; m++) {
-//             if (M == fibArray[i] + fibArray[j] + fibArray[k] + fibArray[l] + fibArray[m]) {
-//               minuteArray.push([fibArray[i], fibArray[j], fibArray[k], fibArray[l], fibArray[m]])
-//             }
-//           }
-//         }
-//       }
-//     }
-//   }
-//   //Checking for the ones
-//   for(let i=0; i < minuteArray[0].length; i++){
-//     if(minuteArray[0][i] === 1){
-//       onesMinuteArray.push(minuteArray[0][i] = 1)
-//     }
-//   }console.log(onesMinuteArray, "ones ")
-//   for(let i=0; i< minuteArray[0].length; i++){
-//     firstMinuteArray.push(minuteArray[0][i])
-//   }
-//   console.log(firstMinuteArray, "this is 1st minute Array")
-//   return firstMinuteArray
-// }
-
